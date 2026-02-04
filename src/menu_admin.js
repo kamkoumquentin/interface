3ee7dbb730a1b3e946306516b405dc428582a023
kamkoumquentin <kamkoumquentin560@gmail.com>
2026-02-04 22:20:16 +0100

    Add Menu component for admin navigation

---
+++ src/menu_admin.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./menu.css";
+
+
+
+function Menu(){
+
+      var naviger= useNavigate();
+
+
+    var page=()=>{
+
+        naviger('/admin_acceuil');
+
+    }
+
+
+     var page1=()=>{
+
+        naviger('/attente');
+
+    }
+
+
+
+
+
+
+return (
+
+    <ul className="liste">
+        <li onClick={page}>cours proposer par les utilisateur</li>
+        <li onClick={page1}>cours validé et en ligne</li>
+    </ul>
+
+);
+
+
+}
+
+
+export default Menu;

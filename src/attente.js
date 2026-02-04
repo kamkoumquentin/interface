f743a425d3e6c29547fd02112ab378df975e22e4
kamkoumquentin <kamkoumquentin560@gmail.com>
2026-02-04 22:19:07 +0100

    Add Attente component for displaying data table

---
+++ src/attente.js
@@ -0,0 +1,140 @@
+import React, { useRef } from "react";
+import { useState } from "react";
+import axios from "axios";
+import './admin.css';
+import { json, useNavigate } from "react-router-dom";
+import {useEffect} from "react"
+
+
+
+function Attente(props,refs){
+
+ var [coeur, setcoeur]=useState('supprimer');
+ var [liste, setliste]=useState([]); 
+ var tab=useRef([]); 
+ var sup= useRef([]);  
+ 
+ var naviger=useNavigate();
+ 
+ var merci=async ()=>{
+ try{
+         var reponse= await axios.get("https://serveur-search.onrender.com/affichage_t");
+           
+            setliste(reponse.data);
+            console.dir(reponse.data);
+           
+   }
+   catch(err){
+
+         console.log('erreur bd '+err);
+   }
+
+   }
+
+useEffect(()=>{
+
+   merci();
+
+
+ },[]);
+
+
+
+
+
+ 
+
+ var valider=(e)=>{
+
+     tab.current.forEach((valeur,index)=>{
+
+          if(e.currentTarget===valeur){
+
+   ( async ()=>{ try{
+         
+         var reponse= await axios.get('https://serveur-search.onrender.com/af/'+valeur.className);
+      
+              var rep= await axios.post("https://serveur-search.onrender.com/ajouter",{...reponse.data});
+            
+            console.dir(reponse.data);
+           
+   }
+   catch(err){
+
+         console.log('erreur bd '+err);
+   }})()
+
+
+}
+
+   })
+     };
+ 
+
+
+  var supprimer=(e)=>{
+
+                console.dir(sup.current);
+ 
+      sup.current.forEach(async (val,index)=>{
+                            
+
+                if(e.currentTarget===val){
+
+                    var reponse= await axios.post("https://serveur-search.onrender.com/supprimer_t",{id:val.className});
+                    console.log(index);
+                    if(reponse){
+                           merci();
+                    }
+                }
+
+      });
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+return(
+     <>
+    <table className="tableau">
+         <thead className="entete">
+         <tr>
+            <th>id</th>
+            <th>nom</th>
+            <th>description</th>
+            <th>lien</th> 
+            <th>action</th>     
+         </tr>
+         </thead>
+         <tbody>
+          {
+
+            liste.map((valeur,index)=>{ 
+               console.log(valeur.nom);
+         return (  <tr key={index}>
+            <td  className="cellule">{valeur.id}</td>    
+            <td className="cellule">{valeur.nom}</td>
+            <td className="cellule">{valeur.description}</td>
+            <td className="cellule">{valeur.lien}</td>
+            <td className="cellule"> <button name="modifier1"  className={valeur.id}  ref={(el)=>(tab.current[index]=el)} onClick={valider} >modifier</button><button  className={valeur.id}  ref={(k)=>(sup.current[index]=k)}   onClick={supprimer} >{coeur}</button><button className="tele" title="lancer le telechargement" ><a href={getDownloadLink(valeur.lien)}  target="_blank" rel="noopener noreferrer" download >  telecharger</a></button></td>
+            </tr>)
+            })
+              } 
+         </tbody>
+        
+      </table>
+
+      </>
+)
+
+}
+
+export default Attente;
